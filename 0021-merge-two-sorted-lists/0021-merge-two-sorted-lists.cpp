d8101f743c6909857acaf2af3a13f0657ee7841d
kartik kashyap <kashyapkartik1022007@gmail.com>
2026-08-22 21:09:18 +0530

    Time: 0 ms (100%), Space: 19.6 MB (27.86%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL || list2 == NULL ){
+            return list1 == NULL ? list2: list1;
+        }
+
+        if(list1->val <= list2->val){
+
+            list1->next= mergeTwoLists(list1->next, list2);
+            return list1;
+        }
+        else{
+            list2->next= mergeTwoLists(list2->next, list1);
+            return list2;
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
