3fde23568aded294e3cf65e702e7c3dc8d76d08c
kartik kashyap <kashyapkartik1022007@gmail.com>
2026-09-07 20:37:09 +0530

    Time: 3 ms (59.93%), Space: 9 MB (43.12%) - LeetHub - Time: O(log₃ n), Space: O(1)

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        long long k=1;
+        while(n>=k){
+            if(n==k){
+                return true;
+            }
+            k=k*3;
+        }
+        return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
