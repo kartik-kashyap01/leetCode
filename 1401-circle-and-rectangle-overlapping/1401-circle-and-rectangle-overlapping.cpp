37529751bf3cc53f3195895e3f1f2eac3e29227f
kartik kashyap <kashyapkartik1022007@gmail.com>
2026-09-19 23:29:13 +0530

    Time: 0 ms (100%), Space: 7.6 MB (95.38%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        int xi;
+        int yi;
+
+        if(x1>xCenter){
+            xi= x1;
+        }
+        else if(x2< xCenter){
+            xi=x2;
+        }
+        else{
+            xi= xCenter;
+        }
+        if(y1>yCenter){
+            yi= y1;
+        }
+        else if(y2< yCenter){
+            yi=y2;
+        }
+        else{
+            yi= yCenter;
+        }
+
+        return sqrt((xi-xCenter)*(xi-xCenter) + (yi-yCenter)*(yi-yCenter))<= radius;
+
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
